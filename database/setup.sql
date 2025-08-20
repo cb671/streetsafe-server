23a96a22e45f3b3dcf87298380eb44b785e8a8ca
OsmanTeklies <155589160+OsmanTeklies@users.noreply.github.com>
2025-08-20 09:54:00 +0100

    added create table code

---
+++ database/setup.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS educational_sources;
+DROP TABLE IF EXISTS emergency_services;
+DROP TABLE IF EXISTS crime_areas;
+
+
+CREATE TABLE emergency_services (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    url VARCHAR(255),
+    h3 BIGINT NOT NULL
+);
+
+CREATE TABLE educational_sources (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(500) NOT NULL,
+    description TEXT NOT NULL,
+    target_crime_type VARCHAR(50) NOT NULL,
+    h3 BIGINT NOT NULL,
+    added_at TIMESTAMP NOT NULL DEFAULT now()
+);
