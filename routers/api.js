695bd1be71241926da4e824f0a8e94f36becb033
cb671 <corymb33@gmail.com>
2025-08-21 10:53:14 +0100

    routing for login/reg

--- routers/api.js
+++ routers/api.js
@@ -1,10 +1,12 @@
 const {Router} = require("express");
 const map = require("./map");
 const graphs = require("./graphs");
+const auth = require("./auth");
 const router = Router();
 
 
 router.use("/map", map);
 router.use("/graphs", graphs);
+router.use("/auth", auth);
 
 module.exports = router;
